27151bd7192e4211a9b6f894cce53bf9dacbfd48
Abhisek Paul <abhisekpaul@Abhiseks-MacBook-Air.local>
2020-08-10 01:24:08 +0530

    Updates

---
+++ precache-manifest.cd817ff9e694b48ac1017d5da9693230.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89364c80b58e5837783292f6adb8dd0e",
+    "url": "/react-ant-app/index.html"
+  },
+  {
+    "revision": "d71f8cf137c66d004582",
+    "url": "/react-ant-app/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "dd45b2f127d149471d00",
+    "url": "/react-ant-app/static/css/main.62d4db45.chunk.css"
+  },
+  {
+    "revision": "d71f8cf137c66d004582",
+    "url": "/react-ant-app/static/js/2.20c49062.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-ant-app/static/js/2.20c49062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd45b2f127d149471d00",
+    "url": "/react-ant-app/static/js/main.7234bf10.chunk.js"
+  },
+  {
+    "revision": "506d99485b88e396c66b",
+    "url": "/react-ant-app/static/js/runtime-main.76fd7994.js"
+  }
+]);
