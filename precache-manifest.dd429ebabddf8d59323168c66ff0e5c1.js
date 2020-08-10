c8c2cc38cbee20d0d3bf142c89fcd65127af5de6
Abhisek Paul <abhisekpaul@Abhiseks-MacBook-Air.local>
2020-08-10 15:05:05 +0530

    Updates

---
+++ precache-manifest.dd429ebabddf8d59323168c66ff0e5c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af2e9fc2c5cf7d78e304f06b78f3d51",
+    "url": "/react-ant-app/index.html"
+  },
+  {
+    "revision": "f334f46d0b2adeb26210",
+    "url": "/react-ant-app/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "5d4b4e5ddfd43a811da0",
+    "url": "/react-ant-app/static/css/main.62d4db45.chunk.css"
+  },
+  {
+    "revision": "f334f46d0b2adeb26210",
+    "url": "/react-ant-app/static/js/2.76e8dcea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-ant-app/static/js/2.76e8dcea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4b4e5ddfd43a811da0",
+    "url": "/react-ant-app/static/js/main.543d2581.chunk.js"
+  },
+  {
+    "revision": "506d99485b88e396c66b",
+    "url": "/react-ant-app/static/js/runtime-main.76fd7994.js"
+  },
+  {
+    "revision": "85e51324558bb02de52c5a552b08f8be",
+    "url": "/react-ant-app/static/media/chief@2x.85e51324.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-ant-app/precache-manifest.26b8813d88ed4dbd300a92b105c88c14.js"
+  "/react-ant-app/precache-manifest.dd429ebabddf8d59323168c66ff0e5c1.js"
 );
 
 self.addEventListener('message', (event) => {
