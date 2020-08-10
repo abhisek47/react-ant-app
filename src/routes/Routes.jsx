22bf25031db8ef7d70d3392279b390a9beb043f0
Abhisek Paul <abhisekpaul@Abhiseks-MacBook-Air.local>
2020-08-10 20:33:23 +0530

    modify routes section

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import PageNotFound from '../components/PageNotFound';
 
 const Routes = () => {
   return (
     <React.Fragment>
-      <Router>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='*' component={PageNotFound} />
+      <Router basename='/'>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='*' component={PageNotFound} />
+        </Switch>
       </Router>
     </React.Fragment>
   );
