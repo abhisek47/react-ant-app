339d34037825673d82d5b705b6116a6fe2860d8d
Abhisek Paul <abhisekpaul@Abhiseks-MacBook-Air.local>
2020-08-12 04:53:45 +0530

    Updates

--- precache-manifest.f7deab891ae04c96da6eaae71cb178f4.js
+++ precache-manifest.f7deab891ae04c96da6eaae71cb178f4.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "66ca41d4c9e29e035cb87de66dfd0a7b",
+    "revision": "669136b5c04cbc9b5cc11cc73471e346",
     "url": "/react-ant-app/index.html"
   },
   {
-    "revision": "51424a92a629f2800c77",
+    "revision": "406f1a306fdfadfbc2b0",
     "url": "/react-ant-app/static/css/2.9c8c8187.chunk.css"
   },
   {
-    "revision": "659b912f9196bc02584a",
+    "revision": "0e1132cedec7d4d4334c",
     "url": "/react-ant-app/static/css/main.2e42de9b.chunk.css"
   },
   {
-    "revision": "51424a92a629f2800c77",
-    "url": "/react-ant-app/static/js/2.45858b32.chunk.js"
+    "revision": "406f1a306fdfadfbc2b0",
+    "url": "/react-ant-app/static/js/2.35acb776.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/react-ant-app/static/js/2.45858b32.chunk.js.LICENSE.txt"
+    "url": "/react-ant-app/static/js/2.35acb776.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "659b912f9196bc02584a",
-    "url": "/react-ant-app/static/js/main.e366e83b.chunk.js"
+    "revision": "0e1132cedec7d4d4334c",
+    "url": "/react-ant-app/static/js/main.53ad2226.chunk.js"
   },
   {
     "revision": "506d99485b88e396c66b",
